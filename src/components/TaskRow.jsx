bb38dd91ea9d8f36d2fc7fdb8c21ec0ba66cd1db
GianniC88 <gianni.colucci.88@gmail.com>
2025-12-03 09:45:31 +0100

    ottimizza TaskRow con React.memo per evitare render inutili

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -1,11 +1,17 @@
-const TaskRow = function ({ task }) {
+import { memo } from "react";
+
+
+const TaskRow = memo(({ task }) => {
+	const status = task.status.replace(" ", "").
+		toLowerCase();
+
 	return (
 		<tr>
 			<td>{task.title}</td>
-			<td>{task.status}</td>
+			<td className={status}>{task.status}</td>
 			<td>{new Date(task.createdAt).toLocaleDateString()}</td>
 		</tr>
 	)
-}
+})
 
 export default TaskRow
