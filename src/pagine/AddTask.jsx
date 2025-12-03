07b5f656d2d0813caaa38b53409637b5a8fa7be7
GianniC88 <gianni.colucci.88@gmail.com>
2025-12-03 14:51:36 +0100

    aggiunge gestione degli errori e validazione nel modulo di aggiunta task

--- src/pagine/AddTask.jsx
+++ src/pagine/AddTask.jsx
@@ -1,19 +1,50 @@
-import { useState, useRef } from "react"
-import TaskList from "./TaskList"
+import { useState, useRef, useMemo } from "react"
+import TaskList from "./TaskList";
+
 
 
 export default function AddTask() {
 	const [title, setTitle] = useState("")
 	const descriptionRef = useRef()
 	const statusRef = useRef()
+	const symbols = "!@#$%^&*()-_=+[]{}|;:'\\\",.<>?/`~";
+
+	const errorName = useMemo(() => {
+		if (!title.trim())
+			return "Assegna un nome alla Task."
+		if ([...title].some(char => symbols.includes(char)))
+			return "Il nome della Task non può contenere simboli"
+		return ""
+	}, [title])
+
+	const handleSubmit = event => {
+		event.preventDefault()
+		if (errorName)
+			return;
+
+		const newTask = {
+			title: title.trim(),
+			description: descriptionRef.current.value,
+			stato: statusRef.current.value
+		}
+		console.log("Task aggiunta:", newTask)
+
+
+		// const handeleSubmit = () => {
+		// 	const task = {
+		// 		title,
+		// 		description: descriptionRef.current.value,
+		// 		status: statusRef.current.value
+		// 	}
+		// 	console.log("task", task)
+
+	}
 	return (
 		<>
 			<div className="add-task">
-				<h1>
-					Agiungi Task
-				</h1>
+				<h1>Agiungi Task</h1>
 				<div className="form-add-task">
-					<form >
+					<form onSubmit={handleSubmit} >
 						<div>
 							<label>
 								<p>Nome Task:</p>
@@ -23,21 +54,33 @@ export default function AddTask() {
 									onChange={e => setTitle(e.target.value)}
 								/>
 							</label>
+							{errorName &&
+								<p className="err-message">{errorName}</p>
+							}
 						</div>
 						<div>
 							<label>
 								<p>Descrizione:</p><textarea ref={descriptionRef} />
-
 							</label>
 						</div>
 						<div>
 							<p>Stato:</p>
-							<select ref={statusRef} defaultValue="" >
+							<select ref={statusRef} defaultValue="To do" required>
+								{["To do", "Doing", "Done"].map((value, index) => {
+									return (
+										<option key={index} value={value} >{value}</option>
+									)
+								})}
+							</select>
+							{/* <select ref={statusRef} defaultValue="" >
 								<option value="" disabled>Seleziona uno stato</option>
 								<option>To do</option>
 								<option>Doing</option>
 								<option>Done</option>
-							</select>
+							</select> */}
+						</div>
+						<div>
+							<button type="submit" disabled={errorName}>Aggiungi Task</button>
 						</div>
 					</form>
 				</div>
