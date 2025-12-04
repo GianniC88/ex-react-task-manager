bbff21d5f4b487d7754f66b2b6773bba456e19a7
GianniC88 <gianni.colucci.88@gmail.com>
2025-12-04 19:14:56 +0100

    aggiunge la funzionalità di ordinamento nella lista dei task con icone di ordinamento

--- src/pagine/TaskList.jsx
+++ src/pagine/TaskList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { GlobalContext } from "../context/GlobalContext"
 import TaskRow from "../components/taskRow"
 
@@ -7,6 +7,19 @@ import TaskRow from "../components/taskRow"
 export default function TaskList() {
 	const { tasks } = useContext(GlobalContext)
 	console.log('Tasks:', tasks)
+
+	const [sortBy, setSortBy] = useState('createdAt')
+	const [sortOrder, setSortOrder] = useState(1)
+	const sortIcon = sortOrder === 1 ? "↑" : "↓"
+
+	const handleSort = (field) => {
+		if (sortBy === field) {
+			setSortOrder(prev => prev * -1)
+		} else {
+			setSortBy(field);
+			setSortOrder(1)
+		}
+	}
 	return (
 		<>
 
@@ -16,9 +29,9 @@ export default function TaskList() {
 					<table >
 						<thead>
 							<tr className="table-row">
-								<th>Nome</th>
-								<th>Status</th>
-								<th>Data di Creazione</th>
+								<th onClick={() => handleSort('title')}>Nome <span className="sort-arrow">{sortBy === "title" && sortIcon}</span></th>
+								<th onClick={() => handleSort('status')}>Status <span className="sort-arrow">{sortBy === "status" && sortIcon}</span></th>
+								<th onClick={() => handleSort('createdAt')}>Data di Creazione <span className="sort-arrow">{sortBy === "createdAt" && sortIcon}</span></th>
 							</tr>
 						</thead>
 						<tbody>
